aae10b5051a38c4097eb2810025372710da8d44f
Paul Paredes <pparedesam@users.noreply.github.com>
2021-03-20 10:45:49 -0500

    jdslñgjksf

---
+++ src/main/java/com/paredespaul/modelo/Usuario.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paredespaul.modelo;
+
+/**
+ *
+ * @author PauPar
+ */
+public class Usuario {
+    
+    private String usuario;
+    private String clave;
+    private String color;
+
+    public Usuario() {
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+    
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 20/03/2021, 10:37:36 AM
+    Author     : PauPar
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
